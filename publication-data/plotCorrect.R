b39f12971b024952b09b8204158cc92800773222
martin-mann <qyu@gmx.de>
2019-11-19 15:06:01 +0100

    plotting of correct predictions

---
+++ publication-data/plotCorrect.R
@@ -0,0 +1,25 @@
+
+
+d = as.data.frame(matrix( 
+dimnames= list( NULL,
+c("length","ring width","max. density","profile consensus","profile set","two step (MD+PS)")),
+c(
+  5, 6.6, 15.8, 19.4, 32.2,NA
+  ,
+  10, 28, 44.8, NA, 58, NA
+  ,
+  15, 55.4, 74.2, NA, 74, NA
+  ,
+  20, 67.6, 84.8, NA, 82.4, NA
+  ,
+  25, 75.4, 86.5, NA, NA, NA
+),ncol=6, byrow=TRUE))
+distCols = !( colnames(d) %in% "length")
+
+matplot(d$length, d[,distCols], lwd=2, type="b", ylim=c(0,100),pch=as.character(1:sum(distCols)),
+        ylab="% correct", xlab="sample length (consecutive years)")
+
+legend("bottomright", legend=colnames(d)[distCols], col = 1:5,
+       pch=as.character(1:sum(distCols)),
+       inset=0.02,
+       title="distance:", title.adj = 0, bty="o", bg="white", box.col="white")
