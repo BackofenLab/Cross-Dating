300b39c0e40d966ce8c54fac19773d1d9b58c2c0
martin-mann <qyu@gmx.de>
2019-11-22 11:38:05 +0100

    plot updated

--- publication-data/plotCorrect.R
+++ publication-data/plotCorrect.R
@@ -10,12 +10,14 @@ c(
   ,
   15, 55.4, 74.2, 57.6, 74, 78.4
   ,
-  20, 67.6, 84.8, NA, 82.4, NA
+  20, 67.6, 84.8, NA, 82.4, 88.0
   ,
-  25, 75.4, 86.5, NA, NA, NA
+  25, 75.4, 86.5, NA, 82.5, 87.3
 ),ncol=6, byrow=TRUE))
 distCols = !( colnames(d) %in% "length")
 
+
+par(family="serif",cex=1.1)
 matplot(d$length, d[,distCols], lwd=2, type="b", ylim=c(0,100),pch=as.character(1:sum(distCols)),
         ylab="% correct", xlab="sample length (consecutive years)")
 
